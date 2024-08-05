a12218773e204e004b4e79dfe89b2003d897f924
roccomilicic <roccomilicic2@gmail.com>
2024-08-05 15:27:09 +1200

    Connectivity with mongo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
+  //console.log('DB_URI:', process.env.DB_URI); // Print the value of DB_URI
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
